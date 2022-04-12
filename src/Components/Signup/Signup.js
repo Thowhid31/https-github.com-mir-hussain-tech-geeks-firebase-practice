1b816b052f5b0bb7710674b190364e5b8e939212
Thowhid31 <thowhidantor16@gmail.com>
2022-04-12 11:27:24 +0600

    added the toast notice

--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,95 +1,158 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import GoogleLogo from "../../Assets/Image/google.svg";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import auth from "../../firebase.init";
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
+import toast from "react-hot-toast";
+import { AiOutlineExclamationCircle } from "react-icons/ai";
 
 const provider = new GoogleAuthProvider();
 
 const Signup = () => {
+  const [email, setEmail] = useState({ value: "", error: "" });
+  const [password, setPassword] = useState({ value: "", error: "" });
+  const [passwordConfirmation, setPasswordConfirmation] = useState({
+    value: "",
+    error: "",
+  });
+
   const navigate = useNavigate();
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
-  console.log(email);
 
   const googleAuth = () => {
     signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      // const credential = GoogleAuthProvider.credentialFromResult(result);
-      // const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      console.log(user);
-      // ...
-      navigate('/')
-    }).catch((error) => {
-      // Handle Errors here.
-      // const errorCode = error.code;
-      // const errorMessage = error.message;
-      // // The email of the user's account used.
-      // const email = error.email;
-      // // The AuthCredential type that was used.
-      // const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-
-    });
-  } 
-
+      .then((result) => {
+        const user = result.user;
+        // ...
+        console.log(user);
+        navigate("/");
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        console.error(error);
+      });
+  };
 
   const handleEmail = (event) => {
-    setEmail(event)
-  }
+    const emailInput = event.target.value;
+    if (/\S+@\S+\.\S+/.test(emailInput)) {
+      setEmail({ value: emailInput, error: "" });
+    } else {
+      setEmail({ value: "", error: "Please Provide a valid Email" });
+    }
+  };
+  const handlePassword = (event) => {
+    const passwordInput = event.target.value;
 
+    if (passwordInput.length < 7) {
+      setPassword({ value: "", error: "Password too short" });
+    } else if (!/(?=.*[A-Z])/.test(passwordInput)) {
+      setPassword({
+        value: "",
+        error: "Password must contain a capital letter",
+      });
+    } else {
+      setPassword({ value: passwordInput, error: "" });
+    }
+  };
+  const handleConfirmPassword = (event) => {
+    const confirmationInput = event.target.value;
 
-  const handleSignOut = (event) => {
+    if (confirmationInput !== password.value) {
+      setPasswordConfirmation({ value: "", error: "Password Mismatched" });
+    } else {
+      setPasswordConfirmation({ value: confirmationInput, error: "" });
+    }
+  };
+
+  const handleSignup = (event) => {
     event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-
-    createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-    console.log(user);
-  })
-  .catch((error) => {
-    // const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-    console.log(errorMessage);
-  });
-  }
+    if (email.value === "") {
+      setEmail({ value: "", error: "Email is required" });
+    }
+    if (password.value === "") {
+      setPassword({ value: "", error: "Password is required" });
+    }
+    if (passwordConfirmation.value === "") {
+      setPasswordConfirmation({
+        value: "",
+        error: "Password confirmation is required",
+      });
+    }
+    if (email.value && password.value === passwordConfirmation.value) {
+      createUserWithEmailAndPassword(auth, email.value, password.value)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          toast.success("Account created", { id: "created" });
+          navigate("/");
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorMessage = error.message;
+          if (errorMessage.includes("already-in-use")) {
+            toast.error("Email already in use", { id: "error" });
+          } else {
+            toast.error(errorMessage, { id: "error" });
+          }
+        });
+    }
+  };
 
   return (
     <div className='auth-form-container '>
       <div className='auth-form'>
         <h1>Sign Up</h1>
-        <form onSubmit={handleSignOut}>
+        <form onSubmit={handleSignup}>
           <div className='input-field'>
             <label htmlFor='email'>Email</label>
             <div className='input-wrapper'>
-              <input onBlur={(event)=>handleEmail(event.target.email.value)}type='email' name='email' id='email' />
+              <input
+                onBlur={handleEmail}
+                type='email'
+                name='email'
+                id='email'
+              />
             </div>
+            {email.error && (
+              <p className='error'>
+                <AiOutlineExclamationCircle /> {email.error}
+              </p>
+            )}
           </div>
           <div className='input-field'>
             <label htmlFor='password'>Password</label>
             <div className='input-wrapper'>
-              <input type='password' name='password' id='password' />
+              <input
+                onBlur={handlePassword}
+                type='password'
+                name='password'
+                id='password'
+              />
             </div>
+            {password.error && (
+              <p className='error'>
+                <AiOutlineExclamationCircle /> {password.error}
+              </p>
+            )}
           </div>
           <div className='input-field'>
             <label htmlFor='confirm-password'>Confirm Password</label>
             <div className='input-wrapper'>
               <input
+                onBlur={handleConfirmPassword}
                 type='password'
                 name='confirmPassword'
                 id='confirm-password'
               />
             </div>
+            {passwordConfirmation.error && (
+              <p className='error'>
+                <AiOutlineExclamationCircle /> {passwordConfirmation.error}
+              </p>
+            )}
           </div>
           <button type='submit' className='auth-form-submit'>
             Sign Up
@@ -105,7 +168,7 @@ const Signup = () => {
           <div className='line-right' />
         </div>
         <div className='input-wrapper'>
-          <button onClick={googleAuth} className='google-auth'>
+          <button className='google-auth' onClick={googleAuth}>
             <img src={GoogleLogo} alt='' />
             <p> Continue with Google </p>
           </button>
